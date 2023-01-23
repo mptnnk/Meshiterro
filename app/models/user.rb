def402b2087300315c9bfeb6f69e4c537c3a315c
mptnnk <121846198+mptnnk@users.noreply.github.com>
2023-01-23 12:19:24 +0000

    meshiterro

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many:post_images,dependent: :destroy
+  #user側がpost_imagesをhas manyである。userが削除されると追随してpost_imagesもdestroyされる
 end
