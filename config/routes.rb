b32917b0764a5e66fc4625c0517b4e659b9700c3
mptnnk <121846198+mptnnk@users.noreply.github.com>
2023-01-27 12:28:57 +0000

    meshiterro

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  get 'homes/about'=>'homes#about',as:"about"
 
   #get 'post_images/new'
   #get 'post_images/index'
   #get 'post_images/show'
-  resources:post_images,only:[:new,:create,:index,:show]
+  resources:post_images,only:[:new,:create,:index,:show,:destroy]
 
-  get 'homes/about'=>'homes#about',as:"about"
+  #get 'users/show'
+  #get 'users/edit'
+  resources:users,only:[:show,:edit,:update]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
